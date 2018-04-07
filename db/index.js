3bfcb9902ceb7b03466c8a1867d93a727d6fc129
Jeremy Philipson <jeremyphilipson@gmail.com>
2018-04-07 14:17:30 -0400

    more students and campuses

--- db/index.js
+++ db/index.js
@@ -3,6 +3,7 @@ const Student = require('./Student');
 const Campus = require('./Campus');
 const conn = require('./conn');
 const faker = require('faker');
+const chance = require('chance')(1234567);
 const avatar = require('cartoon-avatar');
 
 const campus_images = [ '../vendor/images/bu-campus.jpg', '../vendor/images/syracuse-campus.jpg', '../vendor/images/penn-campus.jpg' ]
@@ -14,9 +15,59 @@ const sync = () => {
 Student.belongsTo(Campus);
 Campus.hasMany(Student)
 
+const numStudents = 40;
+const numCampuses = 7;
+
+const doTimes = (n, func) => {
+  const results = []
+  while (n--) {
+    results.push(func())
+  }
+  return results;
+};
+
+function randomStudent() {
+  const gender = chance.gender()
+  const first_name = chance.first({ gender })
+  const last_name = chance.last({ nationality: 'en' })
+  return Student.build({
+    first_name: first_name,
+    last_name: last_name,
+    email: `${first_name.toLowerCase()}.${last_name.toLowerCase()}@school.edu`,
+    gpa: Math.round((Math.random() * 3) * 100) / 100 + 1,
+    image_url: avatar.generate_avatar({ gender: gender })
+  })
+};
+
+function randomCampus() {
+  return Campus.build({
+    name: faker.address.city(),
+    description: faker.lorem.paragraph(),
+    street: chance.address({ short_suffix: true }),
+    city: faker.address.city(),
+    state: chance.state({ country: 'us' }),
+    zip: chance.zip(),
+    image_url: chance.pickone(campus_images)
+  })
+};
+
+const students = doTimes(numStudents, randomStudent)
+const campuses = doTimes(numCampuses, randomCampus)
+
+const seed = () => {
+  return Promise.all(campuses.map(campus => campus.save()))
+    .then(() => Promise.all(students.map(student => {
+      student.save()
+        .then(student => {
+          student.setCampus(chance.pickone(campuses))
+        })
+    }))
+    )
+}
+
 module.exports = {
   sync,
-  conn,
+  seed,
   models: {
     Campus,
     Student
