ca86039556d7f6188dde61d46e8b749645ff5e32
Jeremy Philipson <jeremyphilipson@gmail.com>
2018-04-10 22:24:35 -0400

    final commit

--- src/components/DoNotLook.js
+++ src/components/DoNotLook.js
@@ -4,7 +4,7 @@ const EasterEgg = () => {
   return (
     <div className="jumbotron">
       <blockquote className="blockquote text-center">
-      {/* broken link on purpose */}
+      {/* broken link on purpose :) */}
       <img src="/easter-egg.png" />
       <h3>"It's a feature, not&nbsp;a&nbsp;bug."</h3>
       <footer className="blockquote-footer"><cite title="Source Title">undefined</cite></footer>
